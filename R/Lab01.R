1e5874f2c0dba725b9323a292086bcc0eaf2cc6f
Natalie Larsson <nlarsson@vt.edu>
2023-01-24 01:37:53 +0000

    Problem 1 Completed

--- R/Lab01.R
+++ R/Lab01.R
@@ -1,44 +1,71 @@
-if (!require("pacman")) install.packages("pacman")
-pacman::p_load(ggplot2,dplyr,patchwork,rnoaa)
-print("hello world version 3")
+# Natalie Larsson
+# BSE 5304G
+# Lab 01
+# 1/25/2023
 
-data <- data.frame(
-  day = as.Date("2019-01-01") + 0:99,
-  tmin = runif(100) + seq(1,100)^2.5 / 10000,
-  price = runif(100) + seq(100,1)^1.5 / 10
+print("Hello World")
+
+## SETUP -----
+if (!require("pacman")) install.packages("pacman")
+p_load(rgdal,parallel,ggplot2,dplyr,patchwork,rnoaa,hrbrthemes)
+system("git config --global user.email 'nlarsson@vt.edu' ") 
+system("git config --global user.name 'Natalie Larsson' ")
+
+
+## PROBLEM 1 CODE -----
+
+# hometown: Frederick, Maryland
+ht_lat <- 39.45
+ht_lon <- -77.50
+
+# find stations near Frederick, Maryland
+stns=meteo_distance(
+  station_data=ghcnd_stations(),
+  lat=ht_lat,
+  long=ht_lon,
+  units = "deg",
+  radius = 20,
+  limit = NULL
 )
 
-data$tmax = runif(100)*10  + data$tmin
-coeff <- 6
-# A few constants
-tminColor <- "#0000ff"
-tmaxColor <- "#ff0000"
-priceColor <- rgb(0.2, 0.6, 0.9, 1)
-dir.create("pdfs")
-basestr=format(Sys.time(),"./pdfs/%Y%m%d%H%M")
+# using Sharpsburg, MD station
+# station data only goes through March of 2022 -- OKed by Fuka
+WXData=meteo_pull_monitors(
+  monitors=stns[277,1],   
+  keep_flags = FALSE,
+  date_min = "2016-01-01",
+  date_max = NULL,
+  var = c("TMAX","TMIN","PRCP") 
+)
 
-p1= ggplot(data, aes(x=day)) +
-  geom_line( aes(y=tmin), linewidth=2, color=tminColor) + 
-  geom_line( aes(y=tmax), linewidth=2, color=tmaxColor) + 
-  geom_line( aes(y=price / coeff), linewidth=2, color=priceColor) +
-    scale_y_continuous(
-      # Features of the first axis
-      name = "Temp(C)",
-      
-      # Add a second axis and specify its features
-      sec.axis = sec_axis(~.*coeff, name="Price")
-    ) + 
-    # theme_ipsum() +
-    theme(
-      axis.title.y = element_text(color = "black", size=13),
-      axis.title.y.right = element_text(color = priceColor, size=13)
-    ) +
-    ggtitle("Temperature down, price up")
+#build data frame to preserve original data
+weather <- data.frame(
+  day <- WXData$date,
+  tmin <- WXData$tmin*.1, #covert to celcius from tenth of degree celcius
+  tmax <- WXData$tmax*.1,
+  pr <- WXData$prcp*.01 #convert to cm from tenth of mm
+)
 
-filename=paste0(basestr,"graph01.pdf")
-pdf(filename) 
-plot(p1)
-dev.off()
-print("file size")
-print(file.size(filename))
-print("I finished!")
+# colors
+pr.color <- "#167d7e"
+tmin.color <- "#f57e00"
+tmax.color <-"#FF0000"
+  
+# create ggplot
+weather.chart <- ggplot(weather, aes(x=day)) +
+  geom_line(aes(y=tmin), color=tmin.color, size=1.25) +
+  geom_line(aes(y=tmax), color=tmax.color, size=1.25) +
+  geom_line(aes(y=pr), color=pr.color, size=1.25) +
+  ggtitle("Daily Precipitation, Minimum Temperature, and Maximum Temperature near Frederick, MD") +
+  xlab("Year") + 
+  scale_y_continuous(name="Tempeature (°C)",
+                     sec.axis=sec_axis(~., name="Precipitation (cm)")) +
+  theme_ipsum() +
+  theme(
+    axis.title.y = element_text(color = tmax.color, size=13),
+    axis.title.y.right = element_text(color = pr.color, size=13)
+  )
+
+weather.chart
+
+## PROBLEM 2 CODE -----
